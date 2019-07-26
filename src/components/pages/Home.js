b83df1cef55c7d1d2e594c10e8716a79b30827d0
Alpha Olomi <alphaolomi@gmail.com>
2019-07-26 14:53:24 +0300

    Initial Commit

---
+++ src/components/pages/Home.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const Home = () => (
+	<Fragment>
+		<p className='lead'>
+			This is a standard create-react-app with some modifications
+		</p>
+	</Fragment>
+);
+
+export default Home;
